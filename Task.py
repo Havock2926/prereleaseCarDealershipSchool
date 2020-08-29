568a31f105ac1dfa44d1a63882a5ed083c5c1141
Havock2926 <68291787+Havock2926@users.noreply.github.com>
2020-08-29 21:27:08 +0530

    Add files via upload

---
+++ Task.py
@@ -0,0 +1,13 @@
+#Task 1
+valid_entries = ['y', 'n', 'hatchback', 'estate', 'saloon']
+model_options = {
+	'Hatchback' : 535000,
+	'Saloon' : 495000,
+	'Estate' : 625000
+}
+total_price = 0
+
+is_new = input("Are you a new customer?(y/n)\n> ")
+while is_new.lower not in valid_entries:
+	print("Invalid input\nTry Again")
+	is_new = input("Are you a new customer?(y/n)\n> ")
